5bb470b04bcad5a73ff225394f3799fac29c34b7
Gabie <jacadel1998@hotmail.com>
2020-12-16 09:25:34 +0100

    Self-reference (recursion with nabla operator) and block-based flow control

--- tin.py
+++ tin.py
@@ -12,6 +12,23 @@ class META:
         self.rep = rep
         return self
 
+class BLOCK(META): 
+    def __init__(self, f=None, rep=None): 
+        self.rep = rep
+        self.f = f
+
+    def __call__(self, rep): 
+        def execute_wrapper(prg):
+            def execute_rec(machine, op, ip, tokens, stack):
+                prg.parent = machine
+                stack = prg.execute(stack)
+
+                return ip, tokens, stack
+
+            return execute_rec
+            
+        return BLOCK(execute_wrapper(Tin(rep[1:-1])), rep)
+
 class DEF(META):
     def __call__(self, rep): 
         self.rep = rep
@@ -21,8 +38,8 @@ class DEF(META):
         prg, name = re.match(pattern, rep).groups()
         self.prg = Tin(prg)
         
-        def execute_rec(op, ip, tokens, stack):
-            self.prg.execute(stack)
+        def execute_rec(machine, op, ip, tokens, stack):
+            stack = self.prg.execute(stack)
 
             return ip, tokens, stack
 
@@ -34,33 +51,41 @@ ID = lambda i: lambda _: i
 
 # Meta functions
 
-def dup(op, ip, tokens, stack):
+def dup(machine, op, ip, tokens, stack):
     stack.append(stack[-1])
 
     return ip, tokens, stack
 
-def copy(op, ip, tokens, stack):
+def copy(machine, op, ip, tokens, stack):
     stack[-1] = stack[-(stack[-1] + 1)]
 
     return ip, tokens, stack
 
-def swap(op, ip, tokens, stack):
+def swap(machine, op, ip, tokens, stack):
     stack[-1], stack[-2] = stack[-2], stack[-1]
 
     return ip, tokens, stack
 
-def skip(op, ip, tokens, stack):
-    if stack.pop(): 
-        i += 1
+def skip(machine, op, ip, tokens, stack):
+    if not stack.pop(): 
+        ip += 1
+
+    return ip, tokens, stack
+
+def skip_inv(machine, op, ip, tokens, stack):
+    if stack.pop():   
+        ip += 1
+
+    return ip, tokens, stack
 
 branch_stack = []
 
-def branch_init(op, ip, tokens, stack):
+def branch_init(machine, op, ip, tokens, stack):
     branch_stack.append(ip)    
 
     return ip, tokens, stack
 
-def branch_end(op, ip, tokens, stack):
+def branch_end(machine, op, ip, tokens, stack):
     pos = branch_stack.pop()
 
     if stack.pop(): 
@@ -70,7 +95,7 @@ def branch_end(op, ip, tokens, stack):
 
 loop_stack = []
 
-def foreach_init(op, ip, tokens, stack):
+def foreach_init(machine, op, ip, tokens, stack):
     if loop_stack and loop_stack[-1][0] == ip:
         loop_stack[-1][-1] += 1
         
@@ -81,7 +106,7 @@ def foreach_init(op, ip, tokens, stack):
 
     return ip, tokens, stack
 
-def foreach_end(op, ip, tokens, stack):
+def foreach_end(machine, op, ip, tokens, stack):
     pos, arr, idx = loop_stack[-1]
 
     if idx < len(arr) - 1: 
@@ -94,12 +119,12 @@ def foreach_end(op, ip, tokens, stack):
 
 storer_stack = []
 
-def storer_init(op, ip, tokens, stack):
+def storer_init(machine, op, ip, tokens, stack):
     storer_stack.append(len(stack))    
 
     return ip, tokens, stack
 
-def storer_end(op, ip, tokens, stack):
+def storer_end(machine, op, ip, tokens, stack):
     pos = storer_stack.pop()
 
     arr = np.array(stack[pos:])
@@ -110,7 +135,7 @@ def storer_end(op, ip, tokens, stack):
 
 variables = {}
 
-def define_var(op, ip, tokens, stack):
+def define_var(machine, op, ip, tokens, stack):
     name = op[1:]
 
     variables.setdefault(name, [])
@@ -118,7 +143,7 @@ def define_var(op, ip, tokens, stack):
 
     return ip, tokens, stack
 
-def delete_var(op, ip, tokens, stack):
+def delete_var(machine, op, ip, tokens, stack):
     name = op[1:]
     variables[name].pop()
 
@@ -127,9 +152,18 @@ def delete_var(op, ip, tokens, stack):
 
     return ip, tokens, stack
 
-def get_var(op, ip, tokens, stack):
+def get_var(machine, op, ip, tokens, stack):
     name = op[1:]
     stack.append(variables[name][-1])
+
+    return ip, tokens, stack
+
+def self_reference(machine, op, ip, tokens, stack):
+    if machine.parent:
+        machine.parent.execute(stack)
+
+    else:
+        machine.execute(stack)
 
     return ip, tokens, stack
 
@@ -153,6 +187,7 @@ TOKENS = {
     r'↶': META(swap),
     
     r'\?': META(skip),
+    r'\:': META(skip_inv),
     r'\[': META(branch_init),
     r'\]': META(branch_end),
     r'\{': META(foreach_init),
@@ -160,11 +195,15 @@ TOKENS = {
     r'\(': META(storer_init),
     r'\)': META(storer_end),
 
-    r'\|.+\|→\|.+\|': DEF(lambda i, j, k, l: (j, k, l)),
-
     r'→[a-z_]+': META(define_var),
     r'←[a-z_]+': META(delete_var),
     r'.[a-z_]+': META(get_var),
+
+    r'⟨[^⟨⟩]+⟩': BLOCK(),
+
+    r'\|.+\|→\|.+?\|': DEF(lambda i, j, k, l, m: (k, l, m)),
+
+    r'∇': META(self_reference),
 
     # Functions
     r'\+': ID(lambda i, j: i + j),
@@ -199,6 +238,7 @@ TOKENS = {re.compile(i): j for i, j in TOKENS.items()}
 
 class Tin:
     def __init__(self, code):
+        self.parent = None
         self.tokens, i = [], 0
 
         while i < len(code):
@@ -224,7 +264,7 @@ class Tin:
             tok = self.tokens[ip]
 
             if isinstance(tok, META):
-                ip, self.tokens, stack = tok.f(tok.rep, ip, self.tokens, stack)
+                ip, self.tokens, stack = tok.f(self, tok.rep, ip, self.tokens, stack)
 
             elif callable(tok):
                 arity = tok.__code__.co_argcount
